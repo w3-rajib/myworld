10b017e9b7a3b9141726b61a23dcb2754f8fc900
w3-rajib <85252840+w3-rajib@users.noreply.github.com>
2021-06-03 00:41:11 +0600

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,35 @@
+
+
+x = document.querySelector("title");
+a = document.querySelector(".a");
+b = document.querySelector(".b");
+c = document.querySelector(".c");
+d = document.querySelector(".d");
+e = document.querySelector(".e");
+f = document.querySelector(".f");
+g = document.querySelector(".g");
+h = document.querySelector(".h");
+i = document.querySelector(".i");
+j = document.querySelector(".j");
+k = document.querySelector(".k");
+l = document.querySelector(".l");
+m = document.querySelector(".m");
+n = document.querySelector(".n");
+o = document.querySelector(".o");
+
+x.innerHTML="My World";
+a.innerHTML="Play";
+b.innerHTML="Nursery";
+c.innerHTML="One";
+d.innerHTML="Two";
+e.innerHTML="Three";
+f.innerHTML="Four";
+g.innerHTML="Five";
+h.innerHTML="Six";
+i.innerHTML="Seven";
+j.innerHTML="Eight";
+k.innerHTML="Nine";
+l.innerHTML="Ten";
+m.innerHTML="HSC 1st year";
+n.innerHTML="HSC 2nd year";
+o.innerHTML="Translate";
